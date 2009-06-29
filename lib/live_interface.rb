3a4647474f9f20bddc6f005cf8b35ed9a8a550e2
aberant <qzzzq1@gmail.com>
2009-06-29 17:33:25 -0500

    very basic clip triggering

--- lib/live_interface.rb
+++ lib/live_interface.rb
@@ -1,21 +1,5 @@
-$:.unshift File.join( File.dirname( __FILE__ ), 'rbcoremidi' )
+# $:.unshift File.join( File.dirname( __FILE__ ), 'rbcoremidi' )
+# 
+# require 'midi_in'
 
-require 'midi_in'
 
-# puts CoreMIDI::API.get_sources
-midi_in = MidiIn.new
-midi_in.scan
-midi_in.link(0)
-
-while true
-  if midi_events = midi_in.new_data?
-    midi_events.each do |event| 
-      puts event.data.inspect
-    end
-  end
-end
-# midi_in.capture do |messages|
-#   messages.each do |message|
-#     puts message.inspect
-#   end
-# end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'spec'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'live_interface'
+require 'live_interface/clip'
 
 Spec::Runner.configure do |config|
   
