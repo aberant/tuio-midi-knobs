74981847475f79b75e40e5e7d01fcff61ddd2e7e
aberant <qzzzq1@gmail.com>
2009-07-12 12:18:00 -0500

    adding tests

--- lib/live_interface.rb
+++ lib/live_interface.rb
@@ -9,13 +9,6 @@ require 'live_interface/knob'
 
 require 'tuio_client'
 
-MIDI_CHANNEL = 16
-
-class Numeric
-  def to_deg
-      deg = self.to_f * (180.0/Math::PI)
-  end
-end
 
 class LiveInterface 
   def initialize
