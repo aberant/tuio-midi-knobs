9a33b5e46b9e33ea6ca1d8f0ce74f573dd789c61
aberant <qzzzq1@gmail.com>
2009-07-19 13:28:46 -0500

    Braid: Removed mirror 'lib/rbcoremidi'

--- lib/rbcoremidi/extconf.rb
+++
@@ -1,4 +0,0 @@
-require 'mkmf'
-$CPPFLAGS += " -I/Developer/SDKs/MacOSX10.5.sdk/System/Library/Frameworks/CoreMIDI.framework/Headers"
-$LDFLAGS += " -framework CoreMIDI"
-create_makefile('rbcoremidi')
